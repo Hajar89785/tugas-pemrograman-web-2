566fd81cecb018434d213becc70d27d06319b5eb
Hajar89785 <hajaraswad1473@gmail.com>
2026-04-21 17:47:24 +0800

    feat: penyiapan blade component untuk layout utama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/item', function () {
+    return view('item.index', ['title' => 'Item']);
+});
+
+
+Route::get('/item/create', function () {
+    return view('item.create', ['title' => 'Create Item']);
+});
