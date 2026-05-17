412b980dec698ff83a5687dbd1aa74b6141beb43
Hajar89785 <hajaraswad1473@gmail.com>
2026-05-17 17:57:55 +0800

    feat: Implementasi fungsi hapus data Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -115,6 +115,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete($category);
+        return to_route('category.index')->withSuccess('Data berhasil dihapus');
     }
 }
